50bbf3ac026329c7d72b0e8fb9458bd86f004c41
Hridoy <hridoymuhammad037@gmail.com>
2026-05-08 17:19:26 +0600

    start learng python

---
+++ Python/1.-Arrays-Basics/1.Find Maximum Element.py
@@ -0,0 +1,14 @@
+# Find the maximum element in an array
+def find_max(arr):
+    if not arr:
+        return None
+    max_element = arr[0]
+    for num in arr:
+        if num > max_element:
+            max_element = num
+    return max_element
+
+
+array = [3, 5, 7, 2, 8]
+print("Maximum element:", find_max(array))
+
---
+++ Python/2.String-Basics/1.ReverseString.py
@@ -0,0 +1,10 @@
+# Reverse a String
+## Create a function that takes a string as argument and returns the reversed string.
+## Examples
+## reverse_string("Hello World") ➞ "dlroW olleH"
+def reverse_string(txt):
+    return txt[::-1]
+print(reverse_string("Hello World"))
+
+
+
