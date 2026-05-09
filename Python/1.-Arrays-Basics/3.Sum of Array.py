c5c26a739dde4cf5e8b946e312cd840e636e8766
Hridoy <hridoymuhammad037@gmail.com>
2026-05-09 14:54:02 +0600

    learn array and solve a problem

---
+++ Python/1.-Arrays-Basics/3.Sum of Array.py
@@ -0,0 +1,12 @@
+# Sum of Array Elements
+# example: arr = [10, 5, 8, 20, 3] => output: 46
+
+def Sum(arr):
+    total=0
+    for n in arr:
+        total=+n
+        return total
+    
+arr =[10,5,8,20,3]
+
+print(Sum(arr))
