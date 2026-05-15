ad00bc6c9983abfd418d9fa75f0e4b54d530d3a0
Hridoy <hridoymuhammad037@gmail.com>
2026-05-15 10:48:53 +0600

    learn python

--- Python/1.Arrays-Basics/7.Find the second Largest Element in an array.py
+++ Python/1.Arrays-Basics/7.Find the second Largest Element in an array.py
@@ -1,19 +1,23 @@
 # Find Second Largest Element in an Array
-#example: arr[] = {10, 5, 8, 20, 3} => output: 10
+#example: arr= [10,11, 5, 8, 20, 3] => output: 10
 
 
-def second_largest(arr):
+def second(arr):
+    if not arr:
+        return
     
-    largest=arr[0]
-    second_largest=arr[0]
-    for i in range(1,len(arr)):
-        if arr[i]>largest:
-            second_largest=largest
-            largest=arr[i]
-        elif arr[i]>second_largest and arr[i]!=largest:
-            second_largest=arr[i]
-    return second_largest
+    largest=0
+    second=0
+    
+    for n in arr:
+        if(n > largest):
+            second=largest
+            largest=n
+        elif(n>second and  n!=largest):
+            second=n
+    
+    return second
 
+arr=[10,11,5,8,20,3]
 
-arr=[10, 5, 8, 20, 3]
-print("Second Largest Element is:",second_largest(arr))
+print("The Second Largest number is :",second(arr))
