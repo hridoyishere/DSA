c5bcaa89551b61bf40aea26911b975f908c2da58
Hridoy <hridoymuhammad037@gmail.com>
2026-05-22 10:51:59 +0600

    learned cpp

--- C++/01-Arrays-Basics/5 Reverse an Array.cpp
+++ C++/01-Arrays-Basics/5 Reverse an Array.cpp
@@ -5,29 +5,22 @@
 #include <iostream>
 using namespace std;
 
-void reverseArray(int arr[], int n) {
-    int start = 0;
-    int end = n - 1;
-    while (start < end) {
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
+void Reverse(int arr[], int n) {
+
+    for (int i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
     }
 }
 
 int main() {
+
     int arr[] = {10, 5, 8, 20, 3};
+
     int n = sizeof(arr) / sizeof(arr[0]);
-    cout << "Original array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-    reverseArray(arr, n);
-    cout << "Reversed array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+
+    cout << "Reversed array is: ";
+
+    Reverse(arr, n);
+
     return 0;
 }
