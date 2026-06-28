d2b07fbaa64b93f82ba9b996157406363f487b64
Hridoy <hridoymuhammad037@gmail.com>
2026-06-28 08:29:44 +0600

    solved leetcode problem 1 in js

---
+++ LeetCode/JavaScript/Problem 1.js
@@ -0,0 +1,27 @@
+// Problem 1. Two Sum
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+const sum = (arr, target) => {
+
+    for (let i = 0; i < arr.length - 1; i++) {
+
+        for (let j = i + 1; j < arr.length; j++) {
+
+            if (arr[i] + arr[j] === target) {
+                return [i, j];
+            }
+
+        }
+
+    }
+
+    return "Not Found";
+};
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+console.log(sum(nums, target));
