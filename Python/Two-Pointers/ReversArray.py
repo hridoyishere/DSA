9fc7b1db757b1478748960af33329d03d54699a1
Hridoy <hridoymuhammad037@gmail.com>
2026-09-20 14:17:27 +0600

    update

---
+++ Python/Two-Pointers/ReversArray.py
@@ -0,0 +1,14 @@
+def Revers(arr):
+    left=0
+    right=len(arr)-1
+    while left<right:
+        temp=arr[left]
+        arr[left]=arr[right]
+        arr[right]=temp
+
+        left+=1
+        right-=1
+    return arr
+
+arr=[1,2,3,4,5]
+print(Revers(arr))
